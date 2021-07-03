ddd22ec8b91808e9ef5574fa5aefa3e30e78454a
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2021-07-03 17:44:59 +0200

    clean the logs

--- resources/views/journal_vouchers/table.blade.php
+++ resources/views/journal_vouchers/table.blade.php
@@ -29,11 +29,6 @@
                            class='btn btn-default btn-xs'>
                             <i class="far fa-eye"></i>
                         </a>
-                        {{--                        <a href="{{ route('journalVouchers.edit', [$account,$journalVoucher->id]) }}"--}}
-                        {{--                           class='btn btn-default btn-xs'>--}}
-                        {{--                            <i class="far fa-edit"></i>--}}
-                        {{--                        </a>--}}
-                        {{--                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}--}}
                     </div>
                     {!! Form::close() !!}
                 </td>
